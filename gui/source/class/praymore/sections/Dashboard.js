1e246d469ef96620113f0c92e8c0e1b8ff829b23
Max Taldykin <jorpic@gmail.com>
2011-06-15 17:10:02 +0400

    Наброски навигации с #ссылками

--- gui/source/class/praymore/sections/Dashboard.js
+++ gui/source/class/praymore/sections/Dashboard.js
@@ -10,5 +10,17 @@ qx.Class.define ("praymore.sections.Dashboard",
 		var grid = new qx.ui.layout.Grid;
 		this.setLayout (grid);
 		this.add (new qx.ui.basic.Label ("Dashboard"), {row: 0, column: 0});
+
+		this.add (new qx.ui.basic.Label (). set ({
+			rich: true,
+			value: "<a href=\"#milestones\">Milestones</a>"
+		}),
+		{row: 1, column: 0});
+	},
+
+	members: {
+		getName: function () { return "Dashboard"; },
+		getHash: function () { return "#dashboard"; }
 	}
+
 });
